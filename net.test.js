b24cd4ca4a3b37ff7fe2e43272ec380f58a624b3
Emma Paris <eparis@atlassian.com>
2025-08-19 17:13:27 +0300

    added a negative test

--- net.test.js
+++ net.test.js
@@ -7,6 +7,7 @@ const {
   seatReservationTime,
   seatReservationOne,
   getTextReservation,
+  inactiveButton,
 } = require("./lib/commands.js");
 const { generateName } = require("./lib/util.js");
 
@@ -63,47 +64,43 @@ test("Should show warning if login is not email", async () => {
   await putText(page, 'input[type="email"]', generateName(5));
 });
 
-describe("Website Test 'Идем в Кино", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("http://qamid.tmweb.ru/client/index.php");
-  });
+describe.only("Website Test 'Идем в Кино", () => {
+	beforeEach(async () => {
+		page = await browser.newPage();
+		await page.goto("http://qamid.tmweb.ru/client/index.php");
+	});
 
-  test("Booking a ticket for the Witcher movie in the Modern Hall", async () => {
-    const expected = "Получить код бронирования";
-    await seatReservationData(page, "a:nth-child(2)");
-    await seatReservationTime(
-      page,
-      ".movie-seances__time[href='#'][data-seance-id='225']"
-    );
-    await seatReservationOne(page, "div:nth-child(8) span:nth-child(7)");
-    await page.click(".acceptin-button");
-    const actual = await getTextReservation(page, ".acceptin-button");
-    expect(actual).to.contain(expected);
-  });
-  test.only ("Ticket price for two VIP and standard seats", async () => {
-    const expected = "1500";
-    await seatReservationData(page, "a:nth-child(2)");
-    await seatReservationTime(
-      page,
-      ".movie-seances__time[href='#'][data-seance-id='199']"
-    );
-    await seatReservationOne(page, "div:nth-child(4) span:nth-child(6)");
-    await seatReservationOne(page, "div:nth-child(4) span:nth-child(7)");
-    await page.click(".acceptin-button");
-    const actual = await getTextReservation(page, ".ticket__details.ticket__cost");
-    expect(actual).to.contain(expected);
- });
-test.only ("Attempt to reserve an occupied seat", async () => {
-    await seatReservationData(page, "a:nth-child(4)");
-    await seatReservationTime(
-      page,
-      ".movie-seances__time[href='#'][data-seance-id='199']"
-    );
-    await seatReservationOne(page, "div:nth-child(7) span:nth-child(8)");
-    await page.click(".acceptin-button");
-    const actual = await getTextReservation(page, ".ticket__details.ticket__cost");
-    expect(actual).to.contain(expected);
- });
+	test("Booking a ticket for the Witcher movie in the Modern Hall", async () => {
+		const expected = "Получить код бронирования";
+		await seatReservationData(page, "a:nth-child(2)");
+		await seatReservationTime(
+			page,
+			".movie-seances__time[href='#'][data-seance-id='225']"
+		);
+		await seatReservationOne(page, "div:nth-child(8) span:nth-child(7)");
+		await page.click(".acceptin-button");
+		const actual = await getTextReservation(page, ".acceptin-button");
+		expect(actual).to.contain(expected);
+	});
+
+	test("Ticket price for two VIP and standard seats", async () => {
+		const expected = "1500";
+		await seatReservationData(page, "a:nth-child(2)");
+		await seatReservationTime(
+			page,
+			".movie-seances__time[href='#'][data-seance-id='199']"
+		);
+		await seatReservationOne(page, "div:nth-child(4) span:nth-child(6)");
+		await seatReservationOne(page, "div:nth-child(4) span:nth-child(7)");
+		await page.click(".acceptin-button");
+		const actual = await getTextReservation(page, ".ticket__details.ticket__cost");
+		expect(actual).to.contain(expected);
+	});
+
+	test("Trying to sign up for an expired session", async () => {
+		const actual = await inactiveButton(page, "body > main:nth-child(3) > section:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(1)");
+
+		expect(actual).to.equal(true);
+	});
 
 });
